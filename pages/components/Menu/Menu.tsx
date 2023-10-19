f5bb6499710f00c56e0fd068139c8d6914d611b3
seho <seho.zaq@gmail.com>
2023-10-19 22:42:26 +0800

    feat: init radix system

---
+++ pages/components/Menu/Menu.tsx
@@ -0,0 +1,4 @@
+export default function Menu() {
+  return <menu className="menu min-h-screen w-113px bg-indigo-8"></menu>
+}
+  
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@radix-ui/themes'
+import Menu from './components/Menu/Menu'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-row items-center justify-between">
+      <Menu></Menu>
+    </main>
+  )
+}
