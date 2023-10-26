ccbcbc39ee9c9673249065cdc6bd367b3e279352
seho <seho.zaq@gmail.com>
2023-10-26 23:32:52 +0800

    feat: refinement of eslint and unocss types

--- pages/components/Menu/Menu.tsx
+++ pages/components/Menu/Menu.tsx
@@ -1,17 +1,18 @@
-import {  useState } from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import { useI18n } from '@/locales'
 import Logo from '@/pages/assets/menu/logo.webp'
-import RocketSVG from '@/pages/assets/menu/rocket.svg'
-import PluginSVG from '@/pages/assets/menu/plugin.svg'
-import SettingSVG from '@/pages/assets/menu/setting.svg'
 
+interface MENU_ITEM {
+  label: string
+  icon: string
+}
 
 export default function Menu() {
   const [activeIndex, setActiveIndex] = useState(0)
   const t = useI18n()
 
-  const MENU_ITEMS = [
+  const MENU_ITEMS: MENU_ITEM[] = [
     {
       label: t('nav.project'),
       icon: 'i-ic-round-energy-savings-leaf',
@@ -26,16 +27,20 @@ export default function Menu() {
     },
   ]
 
+  const handleClickMenuItem = (item: MENU_ITEM, index: number) => {
+    setActiveIndex(index)
+  }
+
   return (
     <menu className="menu min-h-screen w-113px bg-indigo-8">
       <div className='mt-53px flex justify-center'>
         <Image src={Logo} width={69} height={90} alt='YourPanel' />
       </div>
-      <nav flex="~ col" className='mt-91px gap-48px'>
+      <nav className='mt-91px gap-48px' flex="~ col">
         {MENU_ITEMS.map((menu, index) => (
-          <div flex="~ col" className='items-center gap-10px cursor-pointer' key={index}>
-            <div flex="center" className='w-38px h-38px rounded-9px transition-all' bg={activeIndex === index ? 'white' : ''}>
-              <div className={`${menu.icon}`} text={`${activeIndex === index ? '2xl black' : '3xl #7067C6'}`}></div>
+          <div className='items-center gap-10px cursor-pointer' flex="~ col" key={index} onClick={() => handleClickMenuItem(menu, index)}>
+            <div className='w-38px h-38px rounded-9px' flex="center" bg={activeIndex === index ? 'white' : ''}>
+              <div className={`${menu.icon} transition-all`} text={`${activeIndex === index ? '2xl black' : '3xl #7067C6'}`} hover={activeIndex === index ? 'text-3xl' : 'text-white/50'}></div>
             </div>
             {index === activeIndex && <span className='text-center color-white'>{menu.label}</span>}
           </div>),
