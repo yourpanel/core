7c04a18d7ad3dea72795f42a3ccdbf95edbb7495
seho <seho.zaq@gmail.com>
2023-12-13 22:06:46 +0800

    feat: add wasmtime integration to plugin-loader

--- packages/plugin-loader/examples/vanilla/src-tauri/src/main.rs
+++ packages/plugin-loader/examples/vanilla/src-tauri/src/main.rs
@@ -3,7 +3,7 @@
 
 fn main() {
   tauri::Builder::default()
-    .plugin(tauri_plugin_plugin_loader::init())
+    .plugin(tauri_plugin_plugin_loader::init().unwrap())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- packages/plugin-loader/src/lib.rs
+++ packages/plugin-loader/src/lib.rs
@@ -1,6 +1,14 @@
 use tauri::{plugin::{Builder, TauriPlugin}, Runtime};
+use wasmtime::*;
 
 /// Initializes the plugin.
-pub fn init<R: Runtime>() -> TauriPlugin<R> {
-  Builder::new("plugin-loader").build()
+pub fn init<R: Runtime>() -> Result<TauriPlugin<R>, wasmtime::Error> {
+  let engine = Engine::default();
+  let module = Module::from_file(&engine, "").unwrap();
+  let mut store = Store::new(&engine, ());
+  let instance = Instance::new(&mut store, &module, &[]).unwrap();
+  let adder = instance.get_typed_func::<(i32, i32), i32>(&mut store, "adder")?;
+  let result = adder.call(&mut store, (1,2))?;
+  println!("result is {:?}", result);
+  Ok(Builder::new("plugin-loader").build())
 }
